843e4d077012734f3f9a56cac3e3c565527aa028
wakame-tech <kamata1919@gmail.com>
2020-07-06 21:05:31 +0900

    :bug: Fix build errors
    # ==================== Emojis ====================
    # 🎉  :tada: 初めてのコミット（Initial Commit）
    # 🔖  :bookmark: バージョンタグ（Version Tag）
    # ✨  :sparkles: 新機能（New Feature）
    # 🐛  :bug: バグ修正（Bagfix）
    # ♻️  :recycle: リファクタリング(Refactoring)
    # 📚  :books: ドキュメント（Documentation）
    # 🎨  :art: デザインUI/UX(Accessibility)
    # 🐎  :horse: パフォーマンス（Performance）
    # 🔧  :wrench: ツール（Tooling）
    # 🚨  :rotating_light: テスト（Tests）
    # 💩  :hankey: 非推奨追加（Deprecation）
    # 🗑️  :wastebasket: 削除（Removal）
    # 🚧  :construction: WIP(Work In Progress)
    
    # ==================== Format ====================
    # :emoji: Subject
    #
    # Commit body...

--- portfolio-theme/layouts/404.vue
+++ portfolio-theme/layouts/404.vue
@@ -5,12 +5,6 @@
         <i class="fas fa-exclamation-triangle" />
         NullReferenceException
       </div>
-
-      <div>
-        <p class="is-5">
-          {{ '{{ ここにテキストを入力 }}' }}
-        </p>
-      </div>
     </div>
   </div>
 </template>
